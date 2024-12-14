e8ce181e21a31acb893578fdbac63caa87ea9ee5
shez222 <bilal.shehroz420@gmail.com>
2024-12-14 10:04:49 +0500

    change

--- admin-panel/src/pages/LandingPage.jsx
+++ admin-panel/src/pages/LandingPage.jsx
@@ -117,7 +117,7 @@ const LandingPage = () => {
             {/* Optional Download Button */}
             
             <a 
-              href="/public/App.apk" 
+              href="/App.apk" 
               className="mt-6 inline-block bg-indigo-600 text-white py-3  rounded-md hover:bg-indigo-700 transition-colors duration-300 text-center lg:w-1/2"
             >
               Download Now ( APK )
